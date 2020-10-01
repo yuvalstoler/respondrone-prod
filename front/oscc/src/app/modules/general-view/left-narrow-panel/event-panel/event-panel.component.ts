3bd85c988002ebf203bcf31439617598a8ab687e
Liza <beta.qween@gmail.com>
2020-10-01 17:05:05 +0300

    changed create new event

--- front/oscc/src/app/modules/general-view/left-narrow-panel/event-panel/event-panel.component.ts
+++ front/oscc/src/app/modules/general-view/left-narrow-panel/event-panel/event-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ApplicationService} from 'src/app/services/applicationService/application.service';
 import {HEADER_BUTTONS} from 'src/types';
 
@@ -44,10 +44,33 @@ export class EventPanelComponent implements OnInit {
     console.log('delete');
     this.clearPanel();
   };
-  
+
   clearPanel = () => {
     this.applicationService.screen.showLeftNarrowPanel = false;
     this.applicationService.selectedHeaderPanelButton = HEADER_BUTTONS.none;
+    this.eventModel = {
+      title: '',
+      type: 'fire',
+      priority: 'normal',
+      description: '',
+      location: {address: '', lat: null, long: null, button: ''},
+      linkedreports: [
+        {ID: 1111, Description: 'Description', Time: 1221321423, Type: 'Fire Alarm'},
+        {ID: 2222, Description: 'Description', Time: 1221344423, Type: 'Fire Alarm'},
+        {ID: 3333, Description: 'Description', Time: 1221333423, Type: 'Road Block'},
+        {ID: 4444, Description: 'Description', Time: 1221352423, Type: 'Fire Alarm'},
+        {ID: 5555, Description: 'Description', Time: 1221324423, Type: 'Road Block'}
+      ],
+      comments: ''
+    };
   }
-  
+
+  onSendComment = () => {
+    if (this.eventModel.comments !== '' && this.eventModel.comments !== undefined) {
+      console.log(this.eventModel.comments);
+      
+      
+    }
+  };
+
 }
