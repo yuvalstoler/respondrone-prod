f80b452d940eb59974609c9ddad80b7bf692ce9d
Omer Porat <poratomer@gmail.com>
2020-10-04 11:02:08 +0300

    DBS API - unfinished

--- back/DBS/src/services/DB/dbManager.ts
+++ back/DBS/src/services/DB/dbManager.ts
@@ -1,10 +1,17 @@
 import {
     ASYNC_RESPONSE,
     ID_OBJ,
-    REPORT_DATA,
+    ID_TYPE,
+    LOG_DATA, LOG_DATA_BY_ID,
+    NFZ_DATA,
+    PERIMETER_TO_SAVE,
+    ROUTE_DATA,
+    SITE_ID_OBJ,
+    TASK,
+    WASP_TASK
 } from '../../../../../classes/typings/all.typings';
 import { PerimeterModel } from '../mongo/models/perimeterModel';
-import { ReportModel } from '../mongo/models/reportModel';
+import { NFZDynamicModel } from '../mongo/models/nfzDynamicModel';
 import { NFZStaticModel } from '../mongo/models/nfzStaticModel';
 import { RouteModel } from '../mongo/models/routeModel';
 import { TaskModel } from '../mongo/models/taskModel';
@@ -20,7 +27,7 @@ export class DbManager {
     private static instance: DbManager = new DbManager();
 
     perimeterModel;
-    reportModel;
+    nfzDynamicModel;
     Logs;
     nfzStaticModel;
     routeModel;
@@ -31,7 +38,7 @@ export class DbManager {
     private constructor() {
         this.perimeterModel = new PerimeterModel().getSchema();
         this.Logs = new LogsModel().getSchema();
-        this.reportModel = new ReportModel().getSchema();
+        this.nfzDynamicModel = new NFZDynamicModel().getSchema();
         this.nfzStaticModel = new NFZStaticModel().getSchema();
         this.routeModel = new RouteModel().getSchema();
         this.taskModel = new TaskModel().getSchema();
@@ -39,14 +46,13 @@ export class DbManager {
     }
 
     // ----------------------
-
-    // ----------------------
-    private setReport = (data: REPORT_DATA): Promise<ASYNC_RESPONSE<REPORT_DATA>> => {
+    private setPerimeter = (data: PERIMETER_TO_SAVE): Promise<ASYNC_RESPONSE> => {
         return new Promise((resolve, reject) => {
-            this.reportModel
+            // const _id = data._id || new (id:MongoConnector.getMongoose()).mongo.ObjectID();
+            this.perimeterModel
                 .findOneAndUpdate({id: data.id}, data, {new: true, upsert: true})
                 .exec()
-                .then((result: REPORT_DATA) => {
+                .then(result => {
                     resolve({success: true, data: result} as ASYNC_RESPONSE);
                 })
                 .catch(error => {
@@ -56,11 +62,96 @@ export class DbManager {
         });
     };
 
-    private readReport = (data: ID_OBJ): Promise<ASYNC_RESPONSE<REPORT_DATA>> => {
+    private readPerimeter = (data: ID_OBJ): Promise<ASYNC_RESPONSE> => {
         return new Promise((resolve, reject) => {
-            this.reportModel.find(data)
+            this.perimeterModel.find(data)
                 .exec()
-                .then((result: REPORT_DATA) => {
+                .then(result => {
+                    const obj = result[0];
+                    resolve({success: true, data: obj || {}} as ASYNC_RESPONSE);
+                })
+                .catch(error => {
+                    console.log(error);
+                    reject({success: false, data: error} as ASYNC_RESPONSE);
+                });
+        });
+    };
+
+    private deletePerimeter = (data: ID_OBJ): Promise<ASYNC_RESPONSE> => {
+        return new Promise((resolve, reject) => {
+            this.perimeterModel
+                .findOneAndDelete(data)
+                .exec()
+                .then(result => {
+                    resolve({success: true, data: result} as ASYNC_RESPONSE);
+                })
+                .catch(error => {
+                    console.log(error);
+                    reject({success: false, data: error} as ASYNC_RESPONSE);
+                });
+        });
+    };
+
+    // ----------------------
+    private setNFZStatic = (data: NFZ_DATA): Promise<ASYNC_RESPONSE<NFZ_DATA>> => {
+        return new Promise((resolve, reject) => {
+            this.nfzStaticModel(data)
+                .save()
+                .then(result => {
+                    resolve({success: true, data: result} as ASYNC_RESPONSE);
+                })
+                .catch(error => {
+                    console.log(error);
+                    reject({success: false, data: error} as ASYNC_RESPONSE);
+                });
+        });
+
+    };
+    private readAllLogData = (data: LOG_DATA): Promise<ASYNC_RESPONSE> => {
+        return new Promise((resolve, reject) => {
+            this.Logs.find(data)
+                .exec()
+                .then(result => {
+                    resolve({success: true, data: result} as ASYNC_RESPONSE);
+                })
+                .catch(error => {
+                    console.log(error);
+                    reject({success: false, data: error} as ASYNC_RESPONSE);
+                });
+        });
+    };
+    private readLogDataById = (data: LOG_DATA_BY_ID): Promise<ASYNC_RESPONSE> => {
+        return new Promise((resolve, reject) => {
+            this.Logs.find( { airVehicleId:data.airVehicleId, timestamp: { $lte: data.endingTimestamp ,  $gte: data.startingTimestamp }})
+                .exec()
+                .then(result => {
+                    resolve({success: true, data: result} as ASYNC_RESPONSE);
+                })
+                .catch(error => {
+                    console.log(error);
+                    reject({success: false, data: error} as ASYNC_RESPONSE);
+                });
+        });
+    };
+    private setLogData = (data: LOG_DATA): Promise<ASYNC_RESPONSE> => {
+        return new Promise((resolve, reject) => {
+            this.Logs(data)
+                .save()
+                .then(result => {
+                    resolve({success: true, data: result} as ASYNC_RESPONSE);
+                })
+                .catch(error => {
+                    console.log(error);
+                    reject({success: false, data: error} as ASYNC_RESPONSE);
+                });
+        });
+
+    };
+    private readNFZStatic = (data: ID_OBJ): Promise<ASYNC_RESPONSE> => {
+        return new Promise((resolve, reject) => {
+            this.nfzStaticModel.find(data)
+                .exec()
+                .then(result => {
                     const obj = result[0];
                     resolve({success: (obj !== undefined), data: obj} as ASYNC_RESPONSE);
                 })
@@ -71,11 +162,11 @@ export class DbManager {
         });
     };
 
-    private readAllReport = (data = {}): Promise<ASYNC_RESPONSE<REPORT_DATA[]>> => {
+    private readAllNFZStatic = (data: SITE_ID_OBJ): Promise<ASYNC_RESPONSE> => {
         return new Promise((resolve, reject) => {
-            this.reportModel.find(data)
+            this.nfzStaticModel.find(data)
                 .exec()
-                .then((result: REPORT_DATA[]) => {
+                .then(result => {
                     resolve({success: true, data: result} as ASYNC_RESPONSE);
                 })
                 .catch(error => {
@@ -85,12 +176,12 @@ export class DbManager {
         });
     };
 
-    private deleteReport = (data: ID_OBJ): Promise<ASYNC_RESPONSE<ID_OBJ>> => {
+    private deleteNFZStatic = (data: ID_OBJ): Promise<ASYNC_RESPONSE<NFZ_DATA>> => {
         return new Promise((resolve, reject) => {
-            this.reportModel
+            this.nfzStaticModel
                 .findOneAndDelete(data)
                 .exec()
-                .then((result: ID_OBJ) => {
+                .then(result => {
                     resolve({success: true, data: result} as ASYNC_RESPONSE);
                 })
                 .catch(error => {
@@ -100,12 +191,12 @@ export class DbManager {
         });
     };
 
-    private deleteAllReport = (data = {}): Promise<ASYNC_RESPONSE<REPORT_DATA>> => {
+    private deleteAllNFZStatic = (data: SITE_ID_OBJ): Promise<ASYNC_RESPONSE<NFZ_DATA>> => {
         return new Promise((resolve, reject) => {
-            this.reportModel
+            this.nfzStaticModel
                 .deleteMany(data)
                 .exec()
-                .then((result: REPORT_DATA) => {
+                .then(result => {
                     resolve({success: true, data: result} as ASYNC_RESPONSE);
                 })
                 .catch(error => {
@@ -115,28 +206,213 @@ export class DbManager {
         });
     };
 
+    // ----------------------
+    private setNFZDynamic = (data: NFZ_DATA): Promise<ASYNC_RESPONSE<NFZ_DATA>> => {
+        return new Promise((resolve, reject) => {
+            this.nfzDynamicModel
+                .findOneAndUpdate({id: data.id}, data, {new: true, upsert: true})
+                .exec()
+                .then(result => {
+                    resolve({success: true, data: result} as ASYNC_RESPONSE);
+                })
+                .catch(error => {
+                    console.log(error);
+                    reject({success: false, data: error} as ASYNC_RESPONSE);
+                });
+        });
+    };
+
+    private readNFZDynamic = (data: ID_OBJ): Promise<ASYNC_RESPONSE> => {
+        return new Promise((resolve, reject) => {
+            this.nfzDynamicModel.find(data)
+                .exec()
+                .then(result => {
+                    const obj = result[0];
+                    resolve({success: (obj !== undefined), data: obj} as ASYNC_RESPONSE);
+                })
+                .catch(error => {
+                    console.log(error);
+                    reject({success: false, data: error} as ASYNC_RESPONSE);
+                });
+        });
+    };
+
+    private readAllNFZDynamic = (data: SITE_ID_OBJ): Promise<ASYNC_RESPONSE> => {
+        return new Promise((resolve, reject) => {
+            this.nfzDynamicModel.find(data)
+                .exec()
+                .then(result => {
+                    resolve({success: true, data: result} as ASYNC_RESPONSE);
+                })
+                .catch(error => {
+                    console.log(error);
+                    reject({success: false, data: error} as ASYNC_RESPONSE);
+                });
+        });
+    };
+
+    private deleteNFZDynamic = (data: ID_OBJ): Promise<ASYNC_RESPONSE<NFZ_DATA>> => {
+        return new Promise((resolve, reject) => {
+            this.nfzDynamicModel
+                .findOneAndDelete(data)
+                .exec()
+                .then(result => {
+                    resolve({success: true, data: result} as ASYNC_RESPONSE);
+                })
+                .catch(error => {
+                    console.log(error);
+                    reject({success: false, data: error} as ASYNC_RESPONSE);
+                });
+        });
+    };
+
+    private deleteAllNFZDynamic = (data: SITE_ID_OBJ): Promise<ASYNC_RESPONSE<NFZ_DATA>> => {
+        return new Promise((resolve, reject) => {
+            this.nfzDynamicModel
+                .deleteMany(data)
+                .exec()
+                .then(result => {
+                    resolve({success: true, data: result} as ASYNC_RESPONSE);
+                })
+                .catch(error => {
+                    console.log(error);
+                    reject({success: false, data: error} as ASYNC_RESPONSE);
+                });
+        });
+    };
 
     // ----------------------
+    private setRoute = (data: ROUTE_DATA): Promise<ASYNC_RESPONSE<ROUTE_DATA>> => {
+        return new Promise((resolve, reject) => {
+            this.routeModel
+                .findOneAndUpdate({routeId: data.routeId}, data, {new: true, upsert: true})
+                .then(result => {
+                    resolve({success: true, data: result} as ASYNC_RESPONSE);
+                })
+                .catch(error => {
+                    console.log(error);
+                    reject({success: false, data: error} as ASYNC_RESPONSE);
+                });
+        });
+
+    };
+
+    private readAllRoutes = (data: SITE_ID_OBJ): Promise<ASYNC_RESPONSE> => {
+        return new Promise((resolve, reject) => {
+            this.routeModel.find(data)
+                .exec()
+                .then(result => {
+                    resolve({success: true, data: result} as ASYNC_RESPONSE);
+                })
+                .catch(error => {
+                    console.log(error);
+                    reject({success: false, data: error} as ASYNC_RESPONSE);
+                });
+        });
+    };
+
+    private deleteRoute = (data: { routeId: ID_TYPE }): Promise<ASYNC_RESPONSE<ROUTE_DATA>> => {
+        return new Promise((resolve, reject) => {
+            this.routeModel
+                .findOneAndDelete(data)
+                .exec()
+                .then(result => {
+                    resolve({success: true, data: result} as ASYNC_RESPONSE);
+                })
+                .catch(error => {
+                    console.log(error);
+                    reject({success: false, data: error} as ASYNC_RESPONSE);
+                });
+        });
+    };
+
+    // ----------------------
+    private setTask = (data: TASK | WASP_TASK): Promise<ASYNC_RESPONSE<TASK | WASP_TASK>> => {
+        return new Promise((resolve, reject) => {
+            this.taskModel
+                .findOneAndUpdate({taskId: data.taskId}, data, {new: true, upsert: true})
+                .then(result => {
+                    resolve({success: true, data: result} as ASYNC_RESPONSE);
+                })
+                .catch(error => {
+                    console.log(error);
+                    reject({success: false, data: error} as ASYNC_RESPONSE);
+                });
+        });
+
+    };
+
+    private readAllTasks = (data: SITE_ID_OBJ): Promise<ASYNC_RESPONSE> => {
+        return new Promise((resolve, reject) => {
+            this.taskModel.find(data)
+                .exec()
+                .then(result => {
+                    resolve({success: true, data: result} as ASYNC_RESPONSE);
+                })
+                .catch(error => {
+                    console.log(error);
+                    reject({success: false, data: error} as ASYNC_RESPONSE);
+                });
+        });
+    };
+
+    private deleteTask = (data: { taskId: ID_TYPE }): Promise<ASYNC_RESPONSE<TASK>> => {
+        return new Promise((resolve, reject) => {
+            this.taskModel
+                .findOneAndDelete({taskId: data.taskId})
+                .exec()
+                .then(result => {
+                    resolve({success: true, data: result} as ASYNC_RESPONSE);
+                })
+                .catch(error => {
+                    console.log(error);
+                    reject({success: false, data: error} as ASYNC_RESPONSE);
+                });
+        });
+    };
+    //---------------------------
+
     private  readAllEvents = (): Promise<ASYNC_RESPONSE<EventClass[]>> => {
         return new Promise(((resolve, reject) => {
             this.eventModel.find()
                 .exec()
                 .then(result => {
-                    // all event in result
-                }).catch(error => {
+                // all event in result
+            }).catch(error => {
                 // error getting events
             })
         }))
     }
 
+
     // region API uncions
+    public static setLogData = DbManager.instance.setLogData;
+    public static readAllLogData = DbManager.instance.readAllLogData;
+    public static readLogDataById = DbManager.instance.readLogDataById;
+    public static setPerimeter = DbManager.instance.setPerimeter;
+    public static readPerimeter = DbManager.instance.readPerimeter;
+    public static deletePerimeter = DbManager.instance.deletePerimeter;
 
-    public static setReport = DbManager.instance.setReport;
-    public static readReport = DbManager.instance.readReport;
-    public static readAllReport = DbManager.instance.readAllReport;
-    public static deleteReport = DbManager.instance.deleteReport;
-    public static deleteAllReport = DbManager.instance.deleteAllReport;
 
+    public static setNFZStatic = DbManager.instance.setNFZStatic;
+    public static readNFZStatic = DbManager.instance.readNFZStatic;
+    public static readAllNFZStatic = DbManager.instance.readAllNFZStatic;
+    public static deleteNFZStatic = DbManager.instance.deleteNFZStatic;
+    public static deleteAllNFZStatic = DbManager.instance.deleteAllNFZStatic;
+
+    public static setNFZDynamic = DbManager.instance.setNFZDynamic;
+    public static readNFZDynamic = DbManager.instance.readNFZDynamic;
+    public static readAllNFZDynamic = DbManager.instance.readAllNFZDynamic;
+    public static deleteNFZDynamic = DbManager.instance.deleteNFZDynamic;
+    public static deleteAllNFZDynamic = DbManager.instance.deleteAllNFZDynamic;
+
+    public static setRoute = DbManager.instance.setRoute;
+    public static readAllRoutes = DbManager.instance.readAllRoutes;
+    public static deleteRoute = DbManager.instance.deleteRoute;
+
+    public static setTask = DbManager.instance.setTask;
+    public static readAllTasks = DbManager.instance.readAllTasks;
+    public static deleteTask = DbManager.instance.deleteTask;
 
     public static readAllEvents = DbManager.instance.readAllEvents;
 
