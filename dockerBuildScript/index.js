f220e675f34f48a8e533c9b4153bd5bc8da091b4
Markin Yuriy <markin.yuriy@gmail.com>
2020-10-09 15:26:27 +0300

    script build dockers

--- dockerBuildScript/index.js
+++ dockerBuildScript/index.js
@@ -126,11 +126,13 @@ for (let prop in names) {
                     dataObj.scripts[keyPackJSON] = fullString;
                 });
 
-                //fs.writeFileSync(pathBase + names[prop].nameOfGroupRepositories + '/' + file, JSON.stringify(dataObj), 'utf8');
+                fs.writeFileSync(pathBase + names[prop].nameOfGroupRepositories + '/' + file, JSON.stringify(dataObj), 'utf8');
 
+            }else {
+              //  fs.writeFileSync(targetPath + names[prop].nameOfGroupRepositories + '/' + names[prop].serviceName + '/' + file);
+                utils.copyFileSync(pathBase + names[prop].nameOfGroupRepositories + '/' + file, targetPath + names[prop].nameOfGroupRepositories + '/' + names[prop].serviceName + '/' + file);
             }
-            fs.writeFileSync(targetPath + names[prop].nameOfGroupRepositories + '/' + names[prop].serviceName + '/' + file);
-            //utils.copyFileSync(pathBase + names[prop].nameOfGroupRepositories + '/' + file, targetPath + names[prop].nameOfGroupRepositories + '/' + names[prop].serviceName + '/' + file);
+            //
         }
     });
 
