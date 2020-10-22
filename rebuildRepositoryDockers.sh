7690e3053b105e89e50aa40a2650f80f4e17b899
Markin Yuriy <markin.yuriy@gmail.com>
2020-10-22 17:27:48 +0300

    script build dockers ad ip of host as arg

--- rebuildRepositoryDockers.sh
+++ rebuildRepositoryDockers.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cd /home/drones/respondrone-master/; 
-git fetch --all; 
-git reset --hard origin/master; 
-cd dockerBuildScript/; 
-node index.js; 
-cd ../../RD; 
-docker-compose down; 
+cd /home/drones/respondrone-master/;
+git fetch --all;
+git reset --hard origin/master;
+cd dockerBuildScript/;
+node index.js ip=192.168.8.100;
+cd ../../RD;
+docker-compose down;
 docker-compose up -d --build
